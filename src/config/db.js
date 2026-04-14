0326929941de905a2dbbfb0e1bee8091a30f6d28
armaangarg73 <armaangarg755@gmail.com>
2026-04-15 04:00:42 +0530

    fix db for render

--- src/config/db.js
+++ src/config/db.js
@@ -1,8 +1,16 @@
 import pg from "pg";
-export const pool = new pg.Pool({
-  host: process.env.DB_HOST || "localhost",
-  port: process.env.DB_PORT || 5432,
-  user: process.env.DB_USER || "postgres",
-  password: process.env.DB_PASSWORD || "postgres",
-  database: process.env.DB_NAME || "sql_db",
-});
+
+export const pool = new pg.Pool(
+  process.env.DATABASE_URL
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false },
+      }
+    : {
+        host: "localhost",
+        port: 5432,
+        user: "postgres",
+        password: "postgres",
+        database: "sql_db",
+      },
+);
